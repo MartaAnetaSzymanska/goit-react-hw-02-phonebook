a9dfda0c451ed1ee99cd78ca0b6b2dffe1f03cd6
MartaAnetaSzymanska <szymanska.ma@gmail.com>
2024-06-14 13:39:27 +0200

    hw-02_zad2.3_done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { Form } from "./Form/Form";
 import { nanoid } from "nanoid";
 import { ContactList } from "./ContactList/ContactList";
+import { Filter } from "./Filter/Filter";
 
 export class App extends Component {
   state = {
@@ -11,21 +12,36 @@ export class App extends Component {
       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
       { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
+    filter: "",
   };
+
   addContact = (name, number) => {
     const newContact = { id: nanoid(), name, number };
     this.setState((prevState) => ({
       contacts: [...prevState.contacts, newContact],
     }));
   };
+
+  setFilter = (filterValue) => {
+    this.setState({ filter: filterValue });
+  };
+
+  filterContact = () => {
+    const { contacts, filter } = this.state;
+    const filterLowerCase = filter.toLowerCase();
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filterLowerCase),
+    );
+  };
   render() {
-    const { contacts } = this.state;
+    const { contacts, filter } = this.state;
     return (
       <>
         <h1>Phonebook</h1>
         <Form contacts={contacts} addContact={this.addContact}></Form>
         <h2>Contacts</h2>
-        <ContactList contacts={contacts}></ContactList>
+        <Filter filter={filter} setFilter={this.setFilter}></Filter>
+        <ContactList filterContact={this.filterContact}></ContactList>
       </>
     );
   }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,18 @@
 import PropTypes from "prop-types";
 
-export const ContactList = ({ contacts }) => {
+export const ContactList = ({ filterContact }) => {
+  const filteredContacts = filterContact();
+
   return (
     <ul>
-      {contacts.map((contact) => (
-        <li key={contact.id}>
-          {contact.name}: {contact.number}
+      {filteredContacts.map((filteredContact) => (
+        <li key={filteredContact.id}>
+          {filteredContact.name}: {filteredContact.number}
         </li>
       ))}
     </ul>
   );
 };
 ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    }),
-  ),
+  filterContact: PropTypes.func.isRequired,
 };
